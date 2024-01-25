7b92c6460d35f92f50284decbdbba1a36a12ca49
jakkaru-devops <savamedvedevvv@gmail.com>
2024-01-25 14:10:10 +0800

    test argocd

--- src/components/complex/Header/index.tsx
+++ src/components/complex/Header/index.tsx
@@ -5,23 +5,14 @@ import { APP_PATHS } from 'data/paths.data';
 import { RoleSwitcher, SearchCatalogInput, Link } from 'components/common';
 import { isUserRequestsBanned, isUserBanned } from 'sections/Users/utils';
 import { useLocale } from 'hooks/locale.hook';
-import {
-  generateInnerUrl,
-  generateUrl,
-  openNotification,
-} from 'utils/common.utils';
+import { generateInnerUrl, generateUrl, openNotification } from 'utils/common.utils';
 import { STRINGS } from 'data/strings.data';
 import { setCartProducts } from 'store/reducers/cart.reducer';
 import { logout } from 'store/reducers/auth.reducer';
 import { useAuth } from 'hooks/auth.hook';
 import { useMessenger } from 'hooks/messenger.hook';
 import { useCart } from 'hooks/cart.hook';
-import {
-  ArrowRightIcon,
-  CartHeaderIcon,
-  CustomRequestMiniIcon,
-  UserHeaderIcon,
-} from 'components/icons';
+import { ArrowRightIcon, CartHeaderIcon, CustomRequestMiniIcon, UserHeaderIcon } from 'components/icons';
 import { Fragment } from 'react';
 import { ANDROID_APP_URL, IOS_APP_URL } from 'data/external.data';
 import { DEFAULT_NAV_PATHS } from 'data/nav.data';
@@ -37,9 +28,8 @@ const Header = () => {
   const handleLogout = () => {
     dispatch(setCartProducts([]));
     dispatch(logout());
-    Object.keys(STRINGS.CUSTOM_ORDER).forEach(key => {
-      if (key !== 'DELIVERY_ADDRESS')
-        localStorage.removeItem(STRINGS.CUSTOM_ORDER[key]);
+    Object.keys(STRINGS.CUSTOM_ORDER).forEach((key) => {
+      if (key !== 'DELIVERY_ADDRESS') localStorage.removeItem(STRINGS.CUSTOM_ORDER[key]);
     });
     router.push(APP_PATHS.HOME);
   };
@@ -50,13 +40,12 @@ const Header = () => {
 
   const isBanned =
     auth.isAuthenticated &&
-    (isUserRequestsBanned(auth.user, auth.currentRole.id) ||
-      isUserBanned(auth.user, auth.currentRole.id));
+    (isUserRequestsBanned(auth.user, auth.currentRole.id) || isUserBanned(auth.user, auth.currentRole.id));
 
   return (
     <Fragment>
       <div className="app-panel">
-        Скачайте наше приложение:
+        Скачайте наше приложение 228:
         <Link href={ANDROID_APP_URL}>
           Google Play <ArrowRightIcon />
         </Link>
@@ -67,11 +56,7 @@ const Header = () => {
       <header className="main-header">
         <div className="container container-fluid">
           <Link href={APP_PATHS.HOME} className="main-header__logo">
-            <img
-              src="/img/logo-large.svg"
-              alt=""
-              className="main-header__logo-img"
-            />
+            <img src="/img/logo-large.svg" alt="" className="main-header__logo-img" />
           </Link>
 
           <SearchCatalogInput />
@@ -85,10 +70,10 @@ const Header = () => {
                 {
                   pathname: APP_PATHS.CUSTOM_ORDER,
                   removeCurrentParams: true,
-                },
+                }
               )}
               className="main-header__custom-request"
-              onClick={e => {
+              onClick={(e) => {
                 if (isBanned) {
                   e.preventDefault();
                   openNotification('Вам запрещенно это действие');
@@ -105,11 +90,10 @@ const Header = () => {
               <Link
                 href={generateUrl(
                   {
-                    [STRINGS.QUERY.SEND_ORDER_REQUEST]:
-                      router.query?.[STRINGS.QUERY.SEND_ORDER_REQUEST],
+                    [STRINGS.QUERY.SEND_ORDER_REQUEST]: router.query?.[STRINGS.QUERY.SEND_ORDER_REQUEST],
                     history: null,
                   },
-                  { pathname: APP_PATHS.LOGIN_CUSTOMER },
+                  { pathname: APP_PATHS.LOGIN_CUSTOMER }
                 )}
                 className="main-header__item link"
               >
@@ -119,7 +103,7 @@ const Header = () => {
 
             {(!isAuthenticated || currentRoleLabel === 'customer') && (
               <Badge
-                count={cart.products.filter(p => !p.priceOfferId).length}
+                count={cart.products.filter((p) => !p.priceOfferId).length}
                 overflowCount={99}
                 size="small"
                 className="main-header__item"
@@ -128,9 +112,7 @@ const Header = () => {
                   href={`${APP_PATHS.REQUEST}?history=request`}
                   className="link"
                   style={isBanned ? { cursor: 'not-allowed' } : {}}
-                  onClick={() =>
-                    isBanned && openNotification('Вам запрещенно это действие')
-                  }
+                  onClick={() => isBanned && openNotification('Вам запрещенно это действие')}
                 >
                   {locale.common.request}
                 </Link>
@@ -171,26 +153,18 @@ const Header = () => {
             )}
 
             {userRolesLength === 1 && (
-              <Link
-                href={APP_PATHS.HOME}
-                onClick={handleLogout}
-                className="main-header__logout main-header__item link"
-              >
+              <Link href={APP_PATHS.HOME} onClick={handleLogout} className="main-header__logout main-header__item link">
                 {locale.user.logout}
               </Link>
             )}
 
             {userRolesLength > 1 && (
-              <RoleSwitcher
-                showBannedRoles
-                handleLogout={handleLogout}
-                className="main-header__item"
-              />
+              <RoleSwitcher showBannedRoles handleLogout={handleLogout} className="main-header__item" />
             )}
 
             {(!isAuthenticated || currentRoleLabel === 'customer') && (
               <Badge
-                count={cart.products.filter(p => p.priceOfferId).length}
+                count={cart.products.filter((p) => p.priceOfferId).length}
                 overflowCount={99}
                 size="small"
                 className="link main-header__item"
@@ -199,7 +173,7 @@ const Header = () => {
                   href={`${APP_PATHS.CART}?history=cart`}
                   className="main-header__cart link"
                   style={isBanned ? { cursor: 'not-allowed' } : null}
-                  onClick={e => {
+                  onClick={(e) => {
                     if (isBanned) {
                       e.preventDefault();
                       openNotification('Вам запрещенно это действие');
