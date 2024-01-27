1fe78148c3e13c9335e32c64ce3ba86ec9537978
jakkaru-devops <savamedvedevvv@gmail.com>
2024-01-27 15:17:27 +0800

    final version frontend

--- src/components/complex/Header/index.tsx
+++ src/components/complex/Header/index.tsx
@@ -45,7 +45,7 @@ const Header = () => {
   return (
     <Fragment>
       <div className="app-panel">
-        «Берсерк» — фэнтези-манга:
+        «One-piece» — фэнтези-манга:
         <Link href={ANDROID_APP_URL}>
           Google Play <ArrowRightIcon />
         </Link>
